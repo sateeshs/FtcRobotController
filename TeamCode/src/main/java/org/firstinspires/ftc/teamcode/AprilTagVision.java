888cc5d7b19fbc7ac8df17a4c37ef86fcd91d7b1
swathysabbineni <swathy.sabbineni@gmail.com>
2023-10-28 23:22:59 -0400

    Move based on April Tag  directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagVision.java
@@ -4,21 +4,48 @@ import android.util.Size;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Mat;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @TeleOp(name = "Utility: Vision April Tag", group = "Utility")
-
 public class AprilTagVision extends LinearOpMode {
 
+    // Adjust these numbers to suit your robot.
+    final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+
+    private DcMotor leftFrontDrive   = null;  //  Used to control the left front drive wheel
+    private DcMotor rightFrontDrive  = null;  //  Used to control the right front drive wheel
+    private DcMotor leftBackDrive    = null;  //  Used to control the left back drive wheel
+    private DcMotor rightBackDrive   = null;  //  Used to control the right back drive wheel
+
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private static final int DESIRED_TAG_ID = 0;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private VisionPortal visionPortal;               // Used to manage the video source.
+    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    boolean targetFound = false;
     final int RESOLUTION_WIDTH = 640;
     final int RESOLUTION_HEIGHT = 480;
     @Override
     public void runOpMode()  {
-        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        leftFrontDrive = hardwareMap.dcMotor.get("motorFrontLeft");
+        leftBackDrive = hardwareMap.dcMotor.get("motorBackLeft");
+        rightFrontDrive = hardwareMap.dcMotor.get("motorFrontRight");
+        rightBackDrive = hardwareMap.dcMotor.get("motorBackRight");
+
+        AprilTagProcessor tagProcessor = null;
+        AprilTagProcessor tagProcessor1 = new AprilTagProcessor.Builder()
                 .setDrawAxes(true)
                 .setDrawCubeProjection(true)
                 .setDrawTagID(true)
@@ -26,35 +53,167 @@ public class AprilTagVision extends LinearOpMode {
                 //.setLensIntrinsics( 502.186,  502.186, 286.502,  316.433) // Calibration points
                 .build();
 
-        VisionPortal visionPortal = new VisionPortal.Builder()
+        AprilTagProcessor tagProcessor2 = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                //.setLensIntrinsics( 502.186,  502.186, 286.502,  316.433) // Calibration points
+                .build();
+
+
+        VisionPortal visionPortal1 = new VisionPortal.Builder()
 
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
                 .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
                 .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
-                .addProcessor(tagProcessor)
+                .addProcessor(tagProcessor1)
+                //.enableCamaraMonitoring(true)
+                .build();
+
+        VisionPortal visionPortal2 = new VisionPortal.Builder()
+
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 2"))
+                .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
+                .addProcessor(tagProcessor2)
                 //.enableCamaraMonitoring(true)
                 .build();
 
         waitForStart();
         while (!isStopRequested() && opModeIsActive())
         {
-
+            //tagProcessor = null;
+            targetFound = false;
+            desiredTag  = null;
             //visionPortal.resumeLiveView();`
-            tagProcessor.getFreshDetections();
+
+            tagProcessor =  tagProcessor1.getDetections().size() ==0 ? tagProcessor2 : tagProcessor1;
+           // tagProcessor.
+            // ();
+
             if(tagProcessor.getDetections().size() >0)
             {
-                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                targetFound = true;
+                desiredTag = tagProcessor.getDetections().get(0);
 
-                telemetry.addData("April x:", tag.ftcPose.x);
-                telemetry.addData("April y:", tag.ftcPose.y);
-                telemetry.addData("April z:", tag.ftcPose.z);
-                telemetry.addData("April roll:", tag.ftcPose.roll);
-                telemetry.addData("April pitch:", tag.ftcPose.pitch);
-                telemetry.addData("April yaw:", tag.ftcPose.yaw);
+                Map<String, Double> directions = directions(desiredTag);
+                //drive, strafe, turn
+                moveRobot(directions.get("drive"),directions.get("strafe"),directions.get("turn"));
+
+                telemetry.addData("April x:", desiredTag.ftcPose.x);
+                telemetry.addData("April y:", desiredTag.ftcPose.y);
+                telemetry.addData("April z:", desiredTag.ftcPose.z);
+                telemetry.addData("April roll:", desiredTag.ftcPose.roll);
+                telemetry.addData("April pitch:", desiredTag.ftcPose.pitch);
+                telemetry.addData("April yaw:", desiredTag.ftcPose.yaw);
             }else{
                 telemetry.addLine("April tag not detected");
             }
             telemetry.update();
         }
     }
+
+    /**
+    * Derive directions to move robot and
+    * Returns drive, strafe, turn values in a dictionary
+     *
+     * * */
+    private  Map<String, Double> directions(AprilTagDetection desiredTag)
+    {
+        //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+        //  applied to the drive motors to correct the error.
+        //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+        final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+        final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+        final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+        final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+        final double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+        final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+        boolean targetFound     = false;    // Set to true when an AprilTag target is detected
+        double  drive           = 0;        // Desired forward power/speed (-1 to +1)
+        double  strafe          = 0;        // Desired strafe power/speed (-1 to +1)
+        double  turn            = 0;        // Desired turning power/speed (-1 to +1)
+
+        // Tell the driver what we see, and what to do.
+        if (targetFound) {
+            telemetry.addData(">"," Drive to Target\n");
+            telemetry.addData("Target", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+            telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+            telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+            telemetry.addData("Yaw","%3.0f degrees", desiredTag.ftcPose.yaw);
+        } else {
+            telemetry.addData(">","target not found \n");
+        }
+
+        // If Left Bumper is being pressed, AND we have found the desired target, Drive to target Automatically .
+        if (targetFound) {
+
+            // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
+            double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+            double  headingError    = desiredTag.ftcPose.bearing;
+            double  yawError        = desiredTag.ftcPose.yaw;
+
+            // Use the speed and turn "gains" to calculate how we want the robot to move.
+            drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+            turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+            strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+
+            telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+        } else {
+
+            // drive using manual POV Joystick mode.  Slow things down to make the robot more controlable.
+            drive  = -gamepad1.left_stick_y  / 2.0;  // Reduce drive rate to 50%.
+            strafe = -gamepad1.left_stick_x  / 2.0;  // Reduce strafe rate to 50%.
+            turn   = -gamepad1.right_stick_x / 3.0;  // Reduce turn rate to 33%.
+            telemetry.addData("Manual","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+        }
+        telemetry.update();
+
+        //drive, strafe, turn
+        Map<String, Double> dictionary = new HashMap<String, Double>();
+        dictionary.put("drive", drive);
+        dictionary.put("strafe", strafe);
+        dictionary.put("turn", turn);
+
+        return  dictionary;
+    }
+
+    /**
+     * Move robot according to desired axes motions
+     * <p>
+     * Positive X is forward
+     * <p>
+     * Positive Y is strafe left
+     * <p>
+     * Positive Yaw is counter-clockwise
+     */
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
+    }
+
 }
