e199f0655c0de9acd9c329d309ec857f20c8212f
swathysabbineni <swathy.sabbineni@gmail.com>
2023-11-01 18:05:11 -0400

    added tflite model and opencv pipeline

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVObjectDetection.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp(name = "OpenCV Object Detection", group = "ftcGamePad")
+public class OpenCVObjectDetection extends OpMode {
+    static final int STREAM_WIDTH = 1920; // modify for your camera
+    static final int STREAM_HEIGHT = 1080; // modify for your camera
+    OpenCvWebcam webcam;
+    SamplePipeline pipeline;
+
+    @Override
+    public void init() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = null;
+        webcamName = hardwareMap.get(WebcamName.class, "WebcamMain"); // put your camera's name here
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        pipeline = new SamplePipeline();
+        webcam.setPipeline(pipeline);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(STREAM_WIDTH, STREAM_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Failed","");
+                telemetry.update();
+            }
+        });
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Image Analysis:",pipeline.getAnalysis());
+        telemetry.update();
+    }
+
+
+}
+
